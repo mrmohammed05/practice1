0772bb64b67f0b22cdc593f9fb7b5f11e1fab870
mohammed <mohammed03074@gmail.com>
2024-12-08 20:01:27 +0530

    first

---
+++ a11.java
@@ -0,0 +1,7 @@
+public class a11{
+    public static void main(String []args){
+        System.out.println("hello world");
+             System.out.println("hello mysore");
+             System.out.println("hello man how are yoye");
+    }
+}
